016dcec3f6c4fd510503625b1f008357bb85e703
Dragan Okanovic <dragan.okan@gmail.com>
2022-05-21 12:06:02 +0200

    Bump content script app to React 18

--- content_scripts/app/src/index.tsx
+++ content_scripts/app/src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 import './bla.scss';
 
-ReactDOM.render(
+createRoot(document.getElementById('content-script-root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('content-script-root'),
 );
