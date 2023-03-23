a40ceee1318394e03a8b7a1e1ac49316768d94a1
Dragan Okanovic <dragan.okan@gmail.com>
2023-03-23 11:10:39 +0100

    Use VIte

---
+++ options/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), cssInjectedByJsPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'build',
+        entryFileNames: 'options-main.js',
+      },
+    },
+  },
+});
